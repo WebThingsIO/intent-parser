5f4ad078bb176b997562e750f6e012cce6e0672c
Michael Stegeman <michael@stegeman.me>
2018-08-29 08:37:15 -0600

    Make server more robust.
    * Define a proper message structure, instead of relying on magic
      characters as separators.
    * Make server threaded.
    * Send proper response messages.
    
    Fixes #1
    Fixes #2

--- intent-parser-server.py
+++ intent-parser-server.py
@@ -1,105 +1,191 @@
 #!/usr/bin/env python3
 
-import socket
-import json
-from adapt.intent import IntentBuilder
+"""
+Intent parser server.
+
+Commands are sent over TCP as follows:
+
+|-------|----------------------------|
+| Bytes | Content                    |
+|-------|----------------------------|
+| 0 - 3 | Length of data, big endian |
+| 4 ... | JSON-encoded data          |
+|-------|----------------------------|
+
+JSON commands are of the form:
+
+{
+    "command": "train|query",
+    "data": ...
+}
+
+For a "train" command, the "data" property is an object of the form:
+
+{
+    "keywords": [ ... ],
+    "types": [ ... ],
+    "locations": [ ... ]
+}
+
+For a "query" command, the "data" property is just the string query.
+"""
+
 from adapt.engine import IntentDeterminationEngine
+from adapt.intent import IntentBuilder
+from socketserver import StreamRequestHandler, ThreadingTCPServer
+import json
+import struct
+import threading
 
 _DEBUG = False
 
 
-def train(keyword, types, locations):
-    global engine
-    engine = IntentDeterminationEngine()
-    for kw in keyword:
-        engine.register_entity(kw, 'Keyword')
+class TCPServer(ThreadingTCPServer):
+    """Threaded TCP server."""
 
-    for t in types:
-        engine.register_entity(t, 'Type')
+    def __init__(self, address, cls):
+        """Initialize the server."""
+        self.allow_reuse_address = True
+        ThreadingTCPServer.__init__(self, address, cls)
 
-    for loc in locations:
-        engine.register_entity(loc, 'Location')
-
-    intent = IntentBuilder('Intent')\
-        .require('Keyword')\
-        .optionally('Type')\
-        .require('Location')\
-        .build()
-
-    engine.register_intent_parser(intent)
+        self.engine = None
+        self.engine_lock = threading.Lock()
 
 
-def print_intent(command):
-    global engine
-    result = ''
-    for intent in engine.determine_intent(command):
-        if intent.get('confidence') > 0:
-            result += json.dumps(intent, indent=4)
-    if result == '':
-        return '-1'
-    else:
-        return result
+class Handler(StreamRequestHandler):
+    """Request handler class."""
 
+    def send_error(self, error):
+        """
+        Send an error to the client.
 
-# next create a socket object
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-port = 5555
+        error -- message to send
+        """
+        self.wfile.write(json.dumps({
+            'status': 'error',
+            'error': error,
+        }).encode('utf-8'))
 
-# Next bind to the port
-s.bind(('127.0.0.1', port))
-if _DEBUG:
-    print('socket binded to %s' % (port))
+    def send_success(self, data=None):
+        """
+        Send a success status to the client.
 
-# put the socket into listening mode
-s.listen(5)
-if _DEBUG:
-    print('socket is listening')
+        data -- optional data to send
+        """
+        message = {
+            'status': 'success',
+        }
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-    try:
-        # Establish connection with client.
-        c, addr = s.accept()
-        if _DEBUG:
-            print('Got connection from', addr)
+        if data is not None:
+            message['data'] = data
 
-        # c.settimeout(5.0)
-        data = c.recv(4096 * 4)
-        # c.settimeout(None)
+        self.wfile.write(json.dumps(message).encode('utf-8'))
 
-        cmd = data.decode('utf-8').strip()
-        if cmd[0] == 't':
-            # received command to train
+    def read_bytes(self, count):
+        """
+        Read bytes from the stream.
+
+        count -- number of bytes to read
+        """
+        data = b''
+        while len(data) < count:
+            data += self.rfile.read(count - len(data))
+
+        return data
+
+    def train(self, keywords, types, locations):
+        """
+        Build and train the intent parser.
+
+        keywords -- list of keywords
+        types -- list of types
+        locations -- list of locations
+        """
+        with self.server.engine_lock:
+            self.server.engine = IntentDeterminationEngine()
+
+            print(keywords, types, locations)
+
+            for kw in keywords:
+                self.server.engine.register_entity(kw, 'Keyword')
+
+            for t in types:
+                self.server.engine.register_entity(t, 'Type')
+
+            for loc in locations:
+                self.server.engine.register_entity(loc, 'Location')
+
+            intent = IntentBuilder('Intent')\
+                .require('Keyword')\
+                .optionally('Type')\
+                .require('Location')\
+                .build()
+
+            self.server.engine.register_intent_parser(intent)
+
+    def query(self, command):
+        """
+        Query the intent parser.
+
+        command -- query string
+        """
+        if self.server.engine is None:
+            return None, 'Intent parser was not trained.'
+
+        result = None
+        with self.server.engine_lock:
+            for intent in self.server.engine.determine_intent(command):
+                if intent.get('confidence') > 0:
+                    result = intent
+                    break
+
+        if result is None:
+            return None, 'Failed to parse command.'
+        else:
+            return result, None
+
+    def handle(self):
+        """Handle an incoming request."""
+        length = struct.unpack('>I', self.read_bytes(4))[0]
+        message = self.read_bytes(length)
+
+        try:
+            message = json.loads(message.decode())
+        except ValueError:
+            self.send_error('Failed to decode message.')
+            return
+
+        if 'command' not in message or 'data' not in message:
+            self.send_error('Invalid message.')
+            return
+
+        data = message['data']
+
+        if message['command'] == 'train':
             if _DEBUG:
-                print('train', cmd[2:].split('|'))
+                print('Train:', json.dumps(data))
 
-            cmd_list = cmd[2:].split('|')
+            if 'keywords' not in data or \
+                    'types' not in data or \
+                    'locations' not in data:
+                self.send_error('Input data is invalid.')
+                return
 
-            keyword_array = []
-            for item in cmd_list[0].split(','):
-                keyword_array.append(item)
-
-            types_array = []
-            for item in cmd_list[1].split(','):
-                types_array.append(item)
-
-            locations_array = []
-            for item in cmd_list[2].split(','):
-                locations_array.append(item)
-
-            train(keyword_array, types_array, locations_array)
-            c.send('1'.encode())
-        elif cmd[0] == 'q':
-            # received a query
+            self.train(data['keywords'], data['types'], data['locations'])
+            self.send_success()
+        elif message['command'] == 'query':
             if _DEBUG:
-                print('query', cmd[2:])
+                print('Query:', data)
 
-            result = print_intent(cmd[2:])
-            c.send(result.encode())
+            result, error = self.query(data)
+            if error:
+                self.send_error(error)
+            else:
+                self.send_success(result)
+        else:
+            self.send_error('Invalid command.')
 
-        c.close()
-    except Exception as e:
-        print('Error:', e)
-        c.close()
+
+if __name__ == '__main__':
+    with TCPServer(('127.0.0.1', 5555), Handler) as server:
+        server.serve_forever()
